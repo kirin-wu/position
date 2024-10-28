27b9b29412fae4e3486e0d34f8ac8336b43c3a88
wukeqi <1270314454qq.com>
2024-10-28 17:27:58 +0800

    map

---
+++ src/views/TencentMap.vue
@@ -0,0 +1,32 @@
+<!--  -->
+<template>
+  <div id="container"></div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    mounted() {
+      this.init()
+    },
+    methods: {
+      init() {
+        console.log(window)
+        var map = new window.TMap.Map(document.getElementById('container'), {
+          center: new window.TMap.LatLng(39.916527, 116.397128),
+          zoom: 11,
+          mapStyleId: 'style1',
+        })
+        console.log(map)
+      },
+    },
+  }
+</script>
+<style scoped lang="less">
+  #container {
+    width: 100%;
+    height: 800px;
+  }
+</style>
